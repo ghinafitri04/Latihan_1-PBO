0a33dd725dd4581588c96d08b2e1f2eafbfb8ae8
ghinafitri04 <100474784+ghinafitri04@users.noreply.github.com>
2022-11-09 21:51:18 +0700

    Update VirtualDemo.java

--- VirtualDemo.java
+++ VirtualDemo.java
@@ -1,7 +1,7 @@
 public class VirtualDemo {
     public static void main(String[] args) {
         Gaji s =  new Gaji("Wahyu", "KUBAR", 3, 5000.00);//upcasting, dilakukan secara internal dan karena upcasting, objek hanya diizinkan untuk mengakses anggota kelas induk dan anggota kelas anak tertentu (metode yang diganti, dll) tetapi tidak semua anggota. 
-        Pegawai e = new Gaji("Ini nama", "Samarinda", 2, 2500.00);//polymorphysm
+        Pegawai e = new Gaji("Ini nama", "Samarinda", 2, 2500.00);//polymorphysm yang mengkonversi dari child class ke parent class
         System.out.println("Memanggil mailCheck Berdasarkan Referensi Gaji --"); 
         s.mailCheck();
         System.out.println("\nMemanggil mailCheck Berdasarkan Referensi Pegawai--");
