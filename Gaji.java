3bed4e92c9015947c9806264863d77c1afce7755
ghinafitri04 <100474784+ghinafitri04@users.noreply.github.com>
2022-11-09 19:33:42 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,10 +1,10 @@
-public class Gaji extends Pegawai {
-    private double salary; //Gaji tahunan
+public class Gaji extends Pegawai {//polymorphysm dari subclass ke superclass
+    private double salary;
     public Gaji(String name, String address, int number, double salary){
         super(name, address, number);
         setSalary (salary);
     }
-    public void mailCheck(){ //polymorphism
+    public void mailCheck(){ //polymorphism upcasting , memiliki nama yg sama dengan superclass (overriding), sehingga bereferensi pada subclass
         System.out.println("Memeriksa kelas gaji dalam surat");
         System.out.println("Surat tertuju untuk" + getName() + "dengan gaji" +  salary);
     }
